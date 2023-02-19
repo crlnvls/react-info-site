69e9448dec8991cac2bb65d2439dd04967066b00
“crlnvls” <cveloso@hotmail.co.uk>
2023-02-19 16:53:23 +0000

    Add page Info

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import * as Pages from "./pages";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Pages.Info />}></Route>
+    </Routes>
+  );
+}
