daaf18cc3bb0e03dc95cf2c5db2bd989c15dec1e
“crlnvls” <cveloso@hotmail.co.uk>
2023-02-19 18:54:05 +0000

    Add footer css

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "./Footer.css";
+
 const Footer = () => {
   return (
     <>
-      <p>
+      <p className="footer">
         2023 Ⓒ
-        <Link to="https://github.com/crlnvls/react-info-site" target="_blank">
+        <Link
+          to="https://github.com/crlnvls/react-info-site"
+          target="_blank"
+          className="link"
+        >
           Caroline Veloso
         </Link>
       </p>
